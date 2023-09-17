0a6de9c91a60888099477f1517ec7c29d8f9a795
Visiok09 <sm3729540@gmail.com>
2023-09-17 22:52:04 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,16 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import './App.css';
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
+import ProductList from "./components/ProductList/ProductList";
+import Form from "./components/Form/Form";
+
+function App() {
+    const {onToggleButton, tg} = useTelegram();
+
+    useEffect(() => {
+        tg.ready();
+    }, [])
+
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path={'form'} element={<Form />}/>
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './App';
+// import { BrowserRouter } from "react-router-dom";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-    <App/>
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//     <App/>
     
-    </BrowserRouter>
+//     </BrowserRouter>
 
-  </React.StrictMode>
-);
+//   </React.StrictMode>
+// );
